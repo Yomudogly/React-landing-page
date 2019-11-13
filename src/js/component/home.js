24a730f2d025aa3c53e2903ddf9267c2a5d27f45
Roman Khalnepesov <khalnepesov@gmail.com>
2019-11-13 02:06:29 +0000

    Card map

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,6 +3,17 @@ import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
 import { Card } from "./card";
 
+let arr = [
+	{
+		src: "https://picsum.photos/500/325",
+		title: "Card title"
+	},
+	{
+		src: "https://picsum.photos/500/325",
+		title: "Card title"
+	}
+];
+
 //create your first component
 export const Home = props => {
 	return (
@@ -10,7 +21,11 @@ export const Home = props => {
 			<Navbar />
 			<div className="container">
 				<Jumbotron />
-				<Card />
+				{arr.map((item, index) => {
+					return (
+						<Card key={index} src={item.src} title={item.title} />
+					);
+				})}
 			</div>
 		</div>
 	);
